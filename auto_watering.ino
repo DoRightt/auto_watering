8d174c976feb3774f01177a0537844f9fe5947fb
cosmicintruder <littlegreenbagg@googlemail.com>
2021-04-21 17:36:20 +0300

    add water-level sensor

---
+++ auto_watering.ino
@@ -0,0 +1,83 @@
+#include <Arduino.h>
+#include "constants.h"
+#include "option.h"
+#include "State.h"
+#include "Controller.h"
+#include "View.h"
+
+#define WATERLEVEL_PIN A4
+
+unsigned long timer;
+
+State state(2, 100, 0, 1, 0);
+View view;
+Controller controller(&state, &view);
+
+void setup() {
+    pinMode(AUTO_LED_PIN, OUTPUT);
+    pinMode(MANUAL_LED_PIN, OUTPUT);
+    pinMode(controller.state_btn.pin, INPUT_PULLUP);
+    pinMode(controller.setting_btn.pin, INPUT_PULLUP);
+    pinMode(controller.down_btn.pin, INPUT_PULLUP);
+    pinMode(controller.up_btn.pin, INPUT_PULLUP);
+    pinMode(controller.cancel_btn.pin, INPUT_PULLUP);
+    pinMode(controller.ok_btn.pin, INPUT_PULLUP);
+
+    timer = millis();
+
+    Serial.begin(9600);
+    state.setStateId(MANUAL_STATE_ID);
+    view.begin(16, 2);
+    view.showScreen(screens::MAIN);
+}
+
+void loop() {
+    checkBtn(controller.state_btn);
+    checkBtn(controller.setting_btn);
+    checkBtn(controller.up_btn);
+    checkBtn(controller.down_btn);
+    checkBtn(controller.cancel_btn);
+    checkBtn(controller.ok_btn);
+
+    checkWaterLevel();
+}
+
+void checkBtn(Button &btn) {
+    bool btn_is_up = digitalRead(btn.pin);
+    if (btn.was_up && !btn_is_up) {
+        if (millis() - timer > 100){
+            timer = millis();
+
+            btn_is_up = digitalRead(btn.pin);
+
+            if (!btn_is_up) {
+                switch (btn.id) {
+                    case buttons::state_button: controller.stateHandler();
+                        break;
+                    case buttons::setting_button: controller.settingHandler();
+                        break;
+                    case buttons::ok_button: controller.okHandler();
+                        break;
+                    case buttons::cancel_button: controller.cancelHandler();
+                        break;
+                    case buttons::up_button: controller.upHandler();
+                        break;
+                    case buttons::down_button: controller.downHandler();
+                        break;
+                }
+            }
+        }
+    }
+
+    btn.was_up = btn_is_up;
+}
+
+void checkWaterLevel() {
+    if (millis() - timer > 1000) {
+        timer = millis();
+
+        int w_level = analogRead(WATERLEVEL_PIN);
+        state.water_level = w_level < 900 ? "low" : "OK";
+        Serial.println(state.water_level);
+    }
+}
